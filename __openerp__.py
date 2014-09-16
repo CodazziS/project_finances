d0a8e670ed0ac5dca1df3aa930cb6e78b166b7ac
stephane- <tempo@codazzi.fr>
2014-09-16 16:18:36 +0200

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Project Finances",
+    'category': 'Project',
+    'description': """
+Project Finances
+===================
+    """,
+    'version': '0.3',
+    'depends': ['account_budget', 'project'],
+    'data': [
+        'static/src/xml/create_project.xml', 
+        'static/src/xml/tree_project.xml', 
+    ],
+}
