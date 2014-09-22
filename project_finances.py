c1ca898b72ed3c2e200ed5e88ddbf4b8dfe153fb
stephane- <tempo@codazzi.fr>
2014-09-22 14:50:25 +0200

    Add panned and practival amount from analytic accounts

--- project_finances.py
+++ project_finances.py
@@ -6,47 +6,38 @@ from lxml import etree as ET
 class project_finances(osv.Model):
     _inherit = 'account.analytic.account'
 
-    def _check_amount(self, amount_expected, parent_id):
-        if parent_id.id > 0:
-            childrens = parent_id.child_complete_ids
-
-            if parent_id.amount_expected > 0:
-                total_amount = amount_expected
-                for children in childrens:
-                    total_amount += children.amount_expected
-                if total_amount > parent_id.amount_expected:
-                    return True
-            return False
-
-    def _count_amount(self, cr, uid, ids, field_name, arg, context):
+    def _count_planned_amount(self, cr, uid, ids, field_name, arg, context):
         res = {}
         record = self.browse(cr, uid, ids, context=context)
         for data in record:
-            amount = 0
-            #res_str = dict(self.pool.get('project_logical_framework.logical_framework').fields_get(cr, uid, allfields=['type'], context=context)['type']['selection'])[data.type]
-            #res_str += "\n" + data.logique
-            res[data.id] = amount
-        
+            planned_amount = 0
+            cross = data.crossovered_budget_line
+            for line in cross:
+                planned_amount += line.planned_amount
+            res[data.id] = planned_amount
         return res
 
-    def _verify_fields(self, cr, uid, ids, field_name, args, context=None):
+    def _count_practical_amount(self, cr, uid, ids, field_name, arg, context):
+        res = {}
         record = self.browse(cr, uid, ids, context=context)
         for data in record:
-            return self._check_amount(data.amount_expected, data.parent_id)
+            practical_amount = 0
+            cross = data.crossovered_budget_line
+            for line in cross:
+                practical_amount += line.practical_amount
+            res[data.id] = practical_amount
+        return res
 
     _columns = {
-        'amount_expected': fields.float(
-            'Amount expected',
-            digits_compute=dp.get_precision('Amount')),
-        'test_amount': fields.function(_count_amount,
-            string="[TEST] Amount expected",
+        'planned_amount': fields.function(_count_planned_amount,
+            string="Planned amount",
             type="float",
             digits_compute=dp.get_precision('Amount')),
-        'amount_overload': fields.function(_verify_fields, type="boolean"),
+        'practical_amount': fields.function(_count_practical_amount,
+            string="Practical amount",
+            type="float",
+            digits_compute=dp.get_precision('Amount')),
      }
-    _defaults = {
-        'amount_expected': 0,
-    }
 
 class project_finances_project(osv.Model):
     _inherit = 'project.project'
